2e6417ec7359ed08b8321bc7295d91fbae1ef424
GruXsqK <HellLord@bk.ru>
2022-01-22 22:10:16 +0300

    Домашняя работа к уроку 8

---
+++ Lesson_8/unit_tests/test_client.py
@@ -0,0 +1,34 @@
+"""Unit-тесты клиента"""
+
+
+import unittest
+from variables import *
+from client import create_presence, process_response_ans
+
+
+class TestClass(unittest.TestCase):
+    """
+    Класс с тестами
+    """
+
+    def test_def_presense(self):
+        """Тест корректного запроса"""
+        test = create_presence()
+        test[TIME] = 1.1
+        self.assertEqual(test, {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}})
+
+    def test_200_ans(self):
+        """Тест корректтного разбора ответа 200"""
+        self.assertEqual(process_response_ans({RESPONSE: 200}), '200 : OK')
+
+    def test_400_ans(self):
+        """Тест корректного разбора 400"""
+        self.assertEqual(process_response_ans({RESPONSE: 400, ERROR: 'Bad Request'}), '400 : Bad Request')
+
+    def test_no_response(self):
+        """Тест исключения без поля RESPONSE"""
+        self.assertRaises(ValueError, process_ans, {ERROR: 'Bad Request'})
+
+
+if __name__ == '__main__':
+    unittest.main()
